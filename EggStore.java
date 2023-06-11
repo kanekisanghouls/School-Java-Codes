2c1d5a3e29e6fb154d6115e9a919168af649bde6
KanekiSanGhouls <101504818+kanekisanghouls@users.noreply.github.com>
2023-06-12 00:47:07 +0800

    Create EggStore.java

---
+++ EggStore.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class EggsCC {
+    public static void main(String[] args) {
+        System.out.print("\n[Title of your Store]");
+        System.out.println("\nPlease enter the number of eggs, \nyou would like to order: ");
+        Scanner scanner = new Scanner(System.in);
+        int numberEggs = scanner.nextInt(); scanner.close();
+        int numberDozens = numberEggs / 12; int numberSingleEggs = numberEggs % 12;
+        double totalCost = numberDozens * 3.25 + numberSingleEggs * 0.45;
+
+        System.out.println("You ordered " + numberEggs + " eggs.");
+        System.out.println("That's " + numberDozens + " dozen at $3.25 per dozen and "
+        + numberSingleEggs + " loose eggs at 45 cents each for a total of $" + totalCost);
+    }
+    /*  / - division
+     *  % - modulo
+     *  * - multiplication
+     *  + - addition
+    */
+    
+}
