836e3b9728c45323827ab2fe129e5e738d5669f2
KanekiSanGhouls <kanekisanghouls@gmail.com>
2023-09-26 22:22:13 +0800

    ㅤ

---
+++ First-Year-Computer Laboratory/Compute/ComputeSquareCircle.java
@@ -0,0 +1,17 @@
+public class ComputeSquareCircle {
+  public static double computeCircumference(double r) {
+    return 2 * Math.PI * r;
+  }
+  public static double computeArea(double r) {
+    return Math.PI * r * r;
+  }
+  public static double computeDiameter(double r) {
+    return 2 * r;
+  }
+  public static double computeSquareArea(double a) {
+    return a * a;
+  }
+  public static double computeSquarePerimeter(double a) {
+    return 4 * a;
+  }
+}
---
+++ First-Year-Computer Laboratory/Metric Conversion/MetricConversion.java
@@ -0,0 +1,14 @@
+public class MetricConversion {
+    
+    public double MeterToCentimeter(int meter) {
+        return meter * 100;
+    }
+    
+    public int FeetToInch(int feet) {
+        return feet * 12;
+    }
+    
+    public double MileToKilometer(double mile) {
+        return mile * 1.61;
+    }
+}
